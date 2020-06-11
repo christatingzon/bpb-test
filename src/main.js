6201c37ec3a7365ee28a94446ad4efc6cecda060
Christa <christatingzon@gmail.com>
2020-06-11 17:59:38 +0800

    Add currency filter

--- src/main.js
+++ src/main.js
@@ -9,6 +9,18 @@ Vue.use(normalize);
 
 Vue.config.productionTip = false;
 
+Vue.filter('currency', function (value) {
+    if (typeof value !== "number") {
+        return value;
+    }
+    var formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2
+    });
+    return formatter.format(value);
+});
+
 new Vue({
   router,
   store,
